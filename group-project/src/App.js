6b27381a377151822586e0a581f92277274d6d5b
KariNurm <karinurm@gmail.com>
2023-01-23 13:10:59 +0200

    Route/Basic structure

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,6 +1,27 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const LandingPage = () => {} ;
+const SearchPage = () => {} ;
+const Header = () => {} ;
+
+
+const App = () => { 
+  
+  
+ return ( 
+  <div className='App'>
+    <Router>
+
+    <Header /> { /* <Link> elements goes to Header component*/ }
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </Router>
   </div>
 )
+}
+
 export default App
--- group-project/src/index.js
+++ group-project/src/index.js
@@ -4,4 +4,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot
-(document.getElementById('root')).render(<App />)
+(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
