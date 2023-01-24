8945c7dbe0e4f587c9dcdcc09a75482ca8999b23
sigdyal <abhinav.sigdyal@tuni.fi>
2023-01-24 10:08:34 +0200

    Update Files for conflict resolve

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,7 +1,24 @@
-import LandingPage from "./LandingPage";
-const App = () => (
-  <div>
-    <LandingPage />
-  </div>
-);
+import LandingPage from "./LandingPage"; /* Landing Page Imported Here */
+import Header from "./Header";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const SearchPage = () => {};
+
+/* Searchpage component, will be imported */
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header /> {/* <Link> elements goes to Header component*/}
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
 export default App;
