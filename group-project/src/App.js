34590dba775211710388f2ea04a73d11960e9f14
KariNurm <karinurm@gmail.com>
2023-01-24 10:30:00 +0200

    added book data axios

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,25 +1,33 @@
-
+import { getBooks } from "./services/axios";
 import Header from "./Header"
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {useEffect, useState } from 'react';
 
 const LandingPage = () => {} ; { /* Landinpage component, will be imported */ }
 const SearchPage = () => {} ; { /* Searchpage component, will be imported */ }
+
 const App = () => { 
+
+useEffect(() => {
+  getBooks().then((data) => {setBooks(data)});
+}, []);
   
+const [books, setBooks] = useState([]);
+console.log(books)
+
  return ( 
   <div className='App'>
-    <Header /> { /* <Link> elements goes to Header component*/ }
     <Router>
-
-   
-
+    
+    <Header /> { /* <Link> elements goes to Header component*/ }
+    
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/search" element={<SearchPage />} />
       </Routes>
     </Router>
-
+      
   </div>
 )
 }
--- group-project/src/Header.js
+++ group-project/src/Header.js
@@ -1,37 +1,16 @@
 import './Header.css';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom"
-
-const Home = () => (
-	<div> <h2>Home</h2> </div>
-)
-const Search = () => (
-	<div> <h2>Search</h2> <code></code></div>
-)
-const Signup = () => (
-	<div> <h2>Signup</h2></div>
-)
-const Login = () => (
-	<div> <h2>Login</h2></div>
-)
+import { BrowserRouter as Link } from "react-router-dom"
 
 const Header = () => {
     const padding = {padding: 15, margin: 15, color: "white"}
 
 	return (
-		<Router>
 			<div className='header'>
 				<Link style={padding} to="/"> HOME </Link>
 				<Link style={padding} to="/search"> SEARCH </Link>
 				<Link style={padding} to="/signup"> SIGNUP </Link>
 				<Link style={padding} to="/login"> LOGIN </Link>
 			</div>
-			<Routes>
-				<Route path="/" element={<Home/>} />
-				<Route path="/search" element={<Search/>} />
-				<Route path="/signup" element={<Signup/>} />
-                <Route path="/login" element={<Login/>} />
-			</Routes>
-		</Router>
 	)
 }
 
