8c5051ad751d740fe4a4289ecb452d20244748c6
KariNurm <karinurm@gmail.com>
2023-01-26 15:35:35 +0200

    add book comp to search list

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,7 +1,6 @@
 import { getBooks } from "./services/Communication";
 import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
-import BookComponent from "./BookComponent";
 import Header from "./Header";
 import LandingPage from "./LandingPage";
 import Login from "./Login"
@@ -12,7 +11,7 @@ import "./App.css";
 const App = () => {
   
 const [books, setBooks] = useState([]);
-console.log(books);
+
 useEffect(() => {
   getBooks().then((data) => {setBooks(data)});
 }, []);
@@ -29,7 +28,6 @@ const toggleForm = (formName) => {
     <Header /> { /* <Link> elements goes to Header component*/ }
   
     <Routes>
-      <Route path="/bookComponent" element={<BookComponent />} />
       <Route path="/" element={<LandingPage />} />
       <Route path="/search" element={<SearchPage books={books}/>} />
       <Route path="/login" element={
--- group-project/src/Header.js
+++ group-project/src/Header.js
@@ -8,7 +8,6 @@ const Header = () => {
 			<div className='header'>
 				<Link className='navLink' to="/"> HOME </Link>
 				<Link className='navLink' to="/search"> SEARCH </Link>
-				<Link className='navLink' to="/bookComponent"> BookComponent </Link> { /* Will be removed, just for testing */}
 			</div>
 	)
 }
