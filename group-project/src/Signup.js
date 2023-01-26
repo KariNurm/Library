875732d49d53f21948e58a131ee843ea400d7351
OlgaKurochkina <>
2023-01-26 15:16:08 +0200

    add Login and Signup style

--- group-project/src/Signup.js
+++ group-project/src/Signup.js
@@ -17,12 +17,12 @@ const Signup = (props) => {
             <h2>Signup</h2>
         <form className="signup-form" onSubmit={handleSubmit}>
             <label htmlFor="name">Name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Name" />
+            <input className="login-input" value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Name" />
             <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <input className="login-input" value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">Password</label>
-            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Submit</button>
+            <input className="login-input" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <button className="login-button" type="submit">Submit</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
