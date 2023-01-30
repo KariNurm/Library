7373a4be27f9f52ea1d478f30bdbd6b9c47fecea
OlgaKurochkina <>
2023-01-30 10:05:33 +0200

    add popup to signup

--- group-project/src/Signup.js
+++ group-project/src/Signup.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
+import { useRef } from "react";
+import Modal from 'react-modal';
 import { v4 as idv4} from 'uuid'
 import "./Login_Signup.css";
+import { Link } from "react-router-dom"
 
 const Signup = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
-
+    const [isOpen, setIsOpen] = useState(false);
+   
+    const customStyles = {
+        content: {
+          inset: '50% auto auto 50%', // top right bottom left
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+      };
+      Modal.setAppElement('#root'); // bind modal to root
+      const subtitle = useRef(null);
+    
+    
     const handleSubmit = (e) => {
         e.preventDefault();
         const newId = idv4();
@@ -19,13 +34,20 @@ const Signup = (props) => {
             admin: false
         }
         props.addNewUser(newUser);
-        
     }
-
     return (
         <div className="container">
-             
             <h2>Signup</h2>
+      <Modal
+        isOpen={isOpen}
+        onAfterOpen={() => {subtitle.style.color = '#f00'}}
+        onRequestClose={() => setIsOpen(false)}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={subtitle}> Thank you for registration! You can <Link onClick={() => props.onFormSwitch('login')}> log in </Link> </h2>
+        <button onClick={() => setIsOpen(false)}> X </button>
+      </Modal>
         <form className="signup-form" onSubmit={handleSubmit}>
             <label htmlFor="name">Name</label>
             <input className="login-input" value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Name" />
@@ -33,7 +55,7 @@ const Signup = (props) => {
             <input className="login-input" value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">Password</label>
             <input className="login-input" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button className="login-button" type="submit">Submit</button>
+            <button className="login-button" onClick={() => setIsOpen(true)} type="submit">Submit</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
