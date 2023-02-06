d94463caadc9b3bf9d718a23b89f4e1ac9d40a67
sigdyal <abhinav.sigdyal@tuni.fi>
2023-02-06 09:03:22 +0200

    Add files

--- group-project/src/MyPage.js
+++ group-project/src/MyPage.js
@@ -1,6 +1,6 @@
 import { UserContext } from "./App";
 import { useContext, useState } from "react";
-import './MyPage.css'
+import "./MyPage.css";
 import Modal from "react-modal";
 Modal.setAppElement("#root");
 
@@ -11,34 +11,39 @@ const MyPage = () => {
   const [isOpen, setIsOpen] = useState(false);
   const borrowedBooks = user.current_loans;
 
-
   return (
     <div className="mypage">
-      <Modal className="mypagemodal"
-                isOpen={isOpen}
-                onRequestClose={() => setIsOpen(false)}
-                contentLabel="MyPage element">
-      <h3>Logged out successfully.</h3>
-      <button onClick={() => setIsOpen(false)}>Close</button>
+      <Modal
+        className="mypagemodal"
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        contentLabel="MyPage element"
+      >
+        <h3>Logged out successfully.</h3>
+        <button onClick={() => setIsOpen(false)}>Close</button>
       </Modal>
-          <h2>Welcome to your page, {user.name}!</h2>
-          {user.current_loans.length === 0 ? <h2>You have no loans</h2>
-                                          : <h2>Your current loans: {borrowedBooks.length}</h2>}
-          {(user.current_loans.length>0) }
-          {borrowedBooks.map (borrowedBook => 
-                  <table className=".myPage table">
-                    <thead>
-                        <tr>
-                          <th>Title</th>
-                          <th>Author</th>
-                        </tr> 
-                        </thead> 
-                    <tr>
-                        <td>{borrowedBook.title}</td>
-                        <td>{borrowedBook.author}</td>
-                        </tr>
-                  </table>)}
+      <h2>Welcome to your page, {user.name}!</h2>
+      {user.current_loans.length === 0 ? (
+        <h2>You have no loans</h2>
+      ) : (
+        <h2>Your current loans: {borrowedBooks.length}</h2>
+      )}
+      {user.current_loans.length > 0}
+      {borrowedBooks.map((borrowedBook) => (
+        <table className="myPage-table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author</th>
+            </tr>
+          </thead>
+          <tr>
+            <td>{borrowedBook.title}</td>
+            <td>{borrowedBook.author}</td>
+          </tr>
+        </table>
+      ))}
     </div>
-  )
-}
+  );
+};
 export default MyPage;
